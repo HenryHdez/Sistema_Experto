0eda11b7c2e4ccf9eb779ca49eafa5973e7f7a87
Henry Alberto Hernandez Martinez <heahernandezma@unal.edu.co>
2020-02-04 08:25:39 -0500

    Update Aplicacion.py

--- Aplicacion_Sistema_Experto/Aplicacion.py
+++ Aplicacion_Sistema_Experto/Aplicacion.py
@@ -47,13 +47,13 @@ Rotulos_Propiedades_de_los_jugos=["Inicial P. Clf [Kg/m3]", "Inicial P. Eva 1 [K
 "Listas de variables"
 Variables_datos_entrada=[]
 Variables_propiedades_jugos=[]
-Variables_Accionamiento=[]
 Variables_Molino=[]
 Variables_Area=[]
+Variables_Accionamiento=[]
 Variables_Q_Etapa=[]
 Calculos_Molino_2=[]
 Calculos_Masas=[]
-
+Cantidad_Etapas=" "
 "Funciones para realizar los calculos de la geometria de la hornilla"
 root = Tk()
 
@@ -70,83 +70,8 @@ def Mostrar_molino(self):
             Variables_Molino[5].set(Carac_Molino_6[j])
             break
 
-"Calculos iniciales"    
-if __name__== "__main__":
-    Helvfont = font.Font(family="Helvetica", size=18, weight="bold")
-    Label(root, text="SISTEMA EXPERTO", font=Helvfont).pack()
-    Label(root, text=" ").pack()
-    Paneles = ttk.Notebook(root)
-    Panel_1 = ttk.Frame(Paneles)
-    Panel_2 = ttk.Frame(Paneles)
-    Panel_3 = ttk.Frame(Paneles)
-    Panel_4 = ttk.Frame(Paneles)
-    Panel_5 = ttk.Frame(Paneles)
-    Panel_6 = ttk.Frame(Paneles)
-    Paneles.add(Panel_1, text='Datos de entrada 1')
-    Paneles.add(Panel_2, text='Datos de entrada 2')
-    Paneles.add(Panel_3, text='Datos de salida 1')
-    Paneles.add(Panel_4, text='Datos de salida 2')
-    Paneles.add(Panel_5, text='Datos de salida 3')
-    Paneles.add(Panel_6, text='Diseño geometrico inicial')
-    "Crear interfaz grafica y calculos iniciales"
-    k=0
-    #Llenado panel 1 Parte 1
-    for i in range(0, len(Carac_Area)):
-        Variables_Area.append(StringVar(value=Carac_Area_i[i]))
-        Label(Panel_1, text=Carac_Area[i]).grid(pady=5, row=i, column=0)
-        Entry(Panel_1, width=20, textvariable=Variables_Area[i]).grid(padx=5, row=i, column=1)    
-        k=k+1
-    
-    #Llenado panel 1 Parte 2
-    j=k
-    for i in range(k, k+len(Titulos_Accionamiento)):
-        Label(Panel_1, text=Titulos_Accionamiento[j-i]).grid(pady=5, row=i, column=0)
-        Variables_Accionamiento.append(StringVar(value=Contenido_Accionamiento[j-i]))
-        Entry(Panel_1, width=20, textvariable=Variables_Accionamiento[j-i]).grid(padx=5, row=i, column=1)
-        k=k+1
-        
-    #Llenado panel 1 Parte 3
-    Variables_Molino.append(StringVar(value=Carac_Molino_1[1]))
-    Variables_Molino.append(StringVar(value=Carac_Molino_2[1]))
-    Variables_Molino.append(StringVar(value=Carac_Molino_3[1]))
-    Variables_Molino.append(StringVar(value=Carac_Molino_4[1]))
-    Variables_Molino.append(StringVar(value=Carac_Molino_5[1]))
-    Variables_Molino.append(StringVar(value=Carac_Molino_6[1]))
-        
-    offset_fila_1=k+len(Titulos_Accionamiento)
-    h1=StringVar(value="Modelo_Molino")
-    b1=ttk.Combobox(Panel_1,width=25,values=Carac_Molino_1[1:], textvariable=h1)
-    b1.bind("<<ComboboxSelected>>", Mostrar_molino)
-    h1.set("R 2-S")
-    b1.grid(pady=5, row=offset_fila_1, column=0)
-    
-    Label(Panel_1, text=Carac_Molino_1[0]).grid(pady=5, row=offset_fila_1+1, column=0)
-    Label(Panel_1, text=Carac_Molino_2[0]).grid(pady=5, row=offset_fila_1+2, column=0)
-    Label(Panel_1, text=Carac_Molino_3[0]).grid(pady=5, row=offset_fila_1+3, column=0)
-    Label(Panel_1, text=Carac_Molino_4[0]).grid(pady=5, row=offset_fila_1+4, column=0)
-    Label(Panel_1, text=Carac_Molino_5[0]).grid(pady=5, row=offset_fila_1+5, column=0)
-    Label(Panel_1, text=Carac_Molino_6[0]).grid(pady=5, row=offset_fila_1+6, column=0)    
-    
-    Entry(Panel_1, width=20, textvariable=Variables_Molino[0]).grid(padx=5, row=offset_fila_1+1, column=1) 
-    Entry(Panel_1, width=20, textvariable=Variables_Molino[1]).grid(padx=5, row=offset_fila_1+2, column=1)
-    Entry(Panel_1, width=20, textvariable=Variables_Molino[2]).grid(padx=5, row=offset_fila_1+3, column=1)
-    Entry(Panel_1, width=20, textvariable=Variables_Molino[3]).grid(padx=5, row=offset_fila_1+4, column=1)
-    Entry(Panel_1, width=20, textvariable=Variables_Molino[4]).grid(padx=5, row=offset_fila_1+5, column=1)
-    Entry(Panel_1, width=20, textvariable=Variables_Molino[5]).grid(padx=5, row=offset_fila_1+6, column=1)    
-       
-    #Llenado panel 2
-    for i in range(0, len(Etiquetas_datos_entrada)):
-        Label(Panel_2, text=Etiquetas_datos_entrada[i]).grid(pady=5, row=i, column=0)
-        if (i!=9):
-            Variables_datos_entrada.append(StringVar(value=Valores_iniciales_datos_entrada[i]))
-            Entry(Panel_2, width=20, textvariable=Variables_datos_entrada[i]).grid(padx=5, row=i, column=1)  
-            Label(Panel_2, text=Unidades_datos_entrada[i]).grid(pady=5, row=i, column=2)
-        else:
-            Variables_datos_entrada.append(StringVar(value="Camara"))
-            ttk.Combobox(Panel_2,width=17,values=["Tpcam", "Ward", "Ad"], textvariable=Variables_datos_entrada[i]).grid(pady=5, row=i, column=1)
-            Variables_datos_entrada[i].set("Tpcam")
-    
-    
+def Actualizar_Valores():
+
     #Llenado panel 3
     G19=float(Variables_datos_entrada[0].get())
     G20=float(Variables_datos_entrada[1].get())
@@ -311,39 +236,196 @@ if __name__== "__main__":
     Variables_Q_Etapa[3].set(Total_Etapa)
     Variables_Q_Etapa[4].set(Total_Etapa_F_L)
     
-    #Contenido del Panel 6
-    Concentracion_solidos_inicial=G24
-    Concentracion_solidos_final=G25
-    Concetracion_promedio=(Concentracion_solidos_inicial+Concentracion_solidos_final)/2
-    Masa_jugo_de_entrada=Cachaza*5
-    Calor_Especifico_P_Cte_jugo=4.18*(1-(0.006*Concetracion_promedio))
-    Densidad_del_Jugo=997.39+(4.46*Concetracion_promedio)
-    Volumen_jugo=Masa_jugo_de_entrada/Densidad_del_Jugo
-    Volumen_jugo_L=Volumen_jugo*1000
-    Temperatura_Entrada=G35
-    Temperatura_Salida=G37+0.2209*math.exp(0.0557*Concentracion_solidos_inicial)
-    Entalpia_Vaporizacion=(2492.9-(2.0523*Temperatura_Entrada))-(0.0030752*(Temperatura_Entrada**2))
-    Masa_Agua_Evaporar=Masa_jugo_de_entrada-(Masa_jugo_de_entrada*Concentracion_solidos_inicial/Concentracion_solidos_final)
-    Calor_por_Etapa=(Masa_jugo_de_entrada*Calor_Especifico_P_Cte_jugo*(Temperatura_Salida-Temperatura_Entrada)+Masa_Agua_Evaporar*Entalpia_Vaporizacion)/3600
+    #Contenido del Panel 6 
+    #Calculo de las demas etapas de la hornilla
+    # Cantidad_Etapas=StringVar(value="7")
+    # Label(Panel_6, text="Cantidad de etapas: ").grid(row=0, column=0)
+    # Entry(Panel_6, width=20, textvariable=Cantidad_Etapas).grid(padx=5, row=0, column=1) 
     
-    print(Concentracion_solidos_inicial)
-    print(Concentracion_solidos_final)
-    print(Concetracion_promedio)
-    print(Masa_jugo_de_entrada)
-    print(Calor_Especifico_P_Cte_jugo)
-    print(Densidad_del_Jugo)
-    print(Volumen_jugo)
-    print(Volumen_jugo_L)
-    print(Temperatura_Entrada)
-    print(Temperatura_Salida)
-    print(Entalpia_Vaporizacion)
-    print(Masa_Agua_Evaporar)
-    print(Calor_por_Etapa)
+    Lista_Contenido=[]
+    Lista_columnas=[]
+    Etapas=int(Cantidad_Etapas.get())
+    if (Etapas>2):
+        Factor_Division=Etapas-2
+    else:
+        Factor_Division=2     
+    #Caracteristicas de las celdas de cada columna
+    #Fila 0 concentración de solidos inicial
+    #Fila 1 Concentración de solidos final
+    #Fila 2 Concentración promedio
+    #Fila 3 Masa de jugo de entrada
+    #Fila 4 Calor Especifico P Cte jugo
+    #Fila 5 Densidad del Jugo
+    #Fila 6 Volumen de jugo kg
+    #Fila 7 Volumen de jugo en L
+    #Fila 8 Temperatura de Entrada
+    #Fila 9 Temperatura de Salida
+    #Fila 10 Entalpia de Vaporización
+    #Fila 11 Masa de Agua a Evaporar
+    #Fila 12 Calor Nece Calc por Etapa
+
+    for i in range(Etapas):
+        for j in range (13):
+            Lista_columnas.append(float(i+j))
+        Lista_Contenido.append(Lista_columnas)
+        Lista_columnas=[]
+    #Suposiciones iniciales
+    Lista_Contenido[0][0]=G26        #Concentracion_solidos_inicial (CSS02)
+    Lista_Contenido[0][1]=G27*100.0  #Concentracion_solidos_final   (CSSF1)
+    Lista_Contenido[Etapas-1][0]=G24 #Concentracion_solidos_inicial (CSS01)
+    Lista_Contenido[Etapas-1][1]=G25 #Concentracion_solidos_final   (CSSF1)
+    ite=0
+    for i in range(Etapas-2,0,-1):
+        Lista_Contenido[i][0]=Lista_Contenido[i+1][1]
+        if(ite==0):
+            Lista_Contenido[i][1]=((Lista_Contenido[0][0]-Lista_Contenido[i][0])/Factor_Division)+Lista_Contenido[i][0]
+            ite=ite+1
+        else:
+            Lista_Contenido[i][1]=((Lista_Contenido[0][0]-Lista_Contenido[Etapas-2][0])/Factor_Division)+Lista_Contenido[i][0]
+               
+    for i in range(Etapas-1,-1,-1):
+        #Concentración promedio=(Concentracion_solidos_inicial+Concentracion_solidos_final)/2
+        Lista_Contenido[i][2]=(Lista_Contenido[i][0]+Lista_Contenido[i][1])/2
+        if(i==Etapas-1):
+            #Masa de jugo de entrada
+            Lista_Contenido[i][3]=A_clarificacion
+        else:
+            #Masa de jugo de entrada=(Masa de jugo etapa anterior*CCS inicial etapa anterior)/CCS Final etapa anterior
+            Lista_Contenido[i][3]=Lista_Contenido[i+1][3]*Lista_Contenido[i+1][0]/Lista_Contenido[i+1][1]    
+        #Calor_Especifico_P_Cte_jugo=4.18*(1-(0.006*Concetracion_promedio))
+        Lista_Contenido[i][4]=4.18*(1-(0.006*Lista_Contenido[i][2]))
+        #Densidad_del_Jugo=997.39+(4.46*Concetracion_promedio)
+        Lista_Contenido[i][5]=997.39+(4.46*Lista_Contenido[i][2])
+        #Volumen_jugo=Masa_jugo_de_entrada/Densidad_del_Jugo
+        Lista_Contenido[i][6]=Lista_Contenido[i][3]/Lista_Contenido[i][5]
+        #Volumen_jugo_L=Volumen_jugo*1000
+        Lista_Contenido[i][7]=Lista_Contenido[i][6]*1000.0
+        if(i==0):
+            #Temperatura_Entrada=Temperatura ambiente
+            Lista_Contenido[i][8]=G34         
+        else:
+            #Temperatura_Entrada=G37+0.2209*math.exp(0.0557*Concentracion_solidos_inicial)
+            Lista_Contenido[i][8]=G37+0.2209*math.exp(0.0557*Lista_Contenido[i][0])
+        if(i==0):    
+            #Temperatura_Salida=Supuesta
+            Lista_Contenido[i][9]=60.0        
+        else:    
+            #Temperatura_Salida=G37+0.2209*math.exp(0.0557*Concentracion_solidos_final)
+            Lista_Contenido[i][9]=G37+0.2209*math.exp(0.0557*Lista_Contenido[i][1])    
+        #Entalpia_Vaporizacion=(2492.9-(2.0523*Temperatura_Entrada))-(0.0030752*(Temperatura_Entrada**2))
+        Lista_Contenido[i][10]=(2492.9-(2.0523*Lista_Contenido[i][8]))-(0.0030752*(Lista_Contenido[i][8]**2))
+        #Masa_Agua_Evaporar=Masa_jugo_de_entrada-(Masa_jugo_de_entrada*Concentracion_solidos_inicial/Concentracion_solidos_final)
+        Lista_Contenido[i][11]=Lista_Contenido[i][3]-(Lista_Contenido[i][3]*Lista_Contenido[i][0]/Lista_Contenido[i][1])
+        #Calor_por_Etapa=(Masa_jugo_de_entrada*Calor_Especifico_P_Cte_jugo*(Temperatura_Salida-Temperatura_Entrada)+Masa_Agua_Evaporar*Entalpia_Vaporizacion)/3600
+        Lista_Contenido[i][12]=(Lista_Contenido[i][3]*Lista_Contenido[i][4]*(Lista_Contenido[i][9]-Lista_Contenido[i][8])+Lista_Contenido[i][11]*Lista_Contenido[i][10])/3600.0
     
-    for i in range (9):
-        for j in range (16):
-            Label(Panel_6, text=" "+str(i)+" "+str(j)).grid(row=i, column=j)
+    #Mostrar en interfaz grafica
+    #Rotulos
+    Label(Panel_6, text="Orden según el flujo de gas").grid(row=1, column=0)
+    Label(Panel_6, text="Concentracion de Solidos Inicial [ºBrix]").grid(row=2, column=0)
+    Label(Panel_6, text="Concentracion de Solidos Final [ºBrix]").grid(row=3, column=0)
+    Label(Panel_6, text="Concentracion de Solidos Promedio [ºBrix]").grid(row=4, column=0)
+    Label(Panel_6, text="Masa de Jugo Entrada [Kg]").grid(row=5, column=0)
+    Label(Panel_6, text="Calor Especifico P Cte jugo [KJ/Kg °C]").grid(row=6, column=0)
+    Label(Panel_6, text="Densidad del Jugo [kg/m3]").grid(row=7, column=0)
+    Label(Panel_6, text="Volumen de jugo [m^3/kg]").grid(row=8, column=0)
+    Label(Panel_6, text="Volumen de jugo [L]").grid(row=9, column=0)
+    Label(Panel_6, text="Temperatura de Entrada [ºC]").grid(row=10, column=0)
+    Label(Panel_6, text="Temperatura de Salida [ºC]").grid(row=11, column=0)
+    Label(Panel_6, text="Entalpia de Vaporización [KJ/kg]").grid(row=12, column=0)
+    Label(Panel_6, text="Masa de Agua a Evaporar [Kg]").grid(row=13, column=0)
+    Label(Panel_6, text="Calor Nece Calc por Etapa [KW]").grid(row=14, column=0)
     
+    for i in range (Etapas):
+        Label(Panel_6, text=" "+str(i+1)).grid(row=1, column=i+1)
+        
+    for i in range (13):
+        for j in range (Etapas):
+            Label(Panel_6, text=" "+str(round(Lista_Contenido[j][i],3))).grid(row=i+2, column=j+1)
+
+
+"Calculos iniciales"    
+if __name__== "__main__":
+    Helvfont = font.Font(family="Helvetica", size=18, weight="bold")
+    Label(root, text="SISTEMA EXPERTO", font=Helvfont).pack()
+    Label(root, text=" ").pack()
+    Paneles = ttk.Notebook(root)
+    Panel_1 = ttk.Frame(Paneles)
+    Panel_2 = ttk.Frame(Paneles)
+    Panel_3 = ttk.Frame(Paneles)
+    Panel_4 = ttk.Frame(Paneles)
+    Panel_5 = ttk.Frame(Paneles)
+    Panel_6 = ttk.Frame(Paneles)
+    Paneles.add(Panel_1, text='Datos de entrada 1')
+    Paneles.add(Panel_2, text='Datos de entrada 2')
+    Paneles.add(Panel_3, text='Datos de salida 1')
+    Paneles.add(Panel_4, text='Datos de salida 2')
+    Paneles.add(Panel_5, text='Datos de salida 3')
+    Paneles.add(Panel_6, text='Diseño geometrico inicial')
+    "Crear interfaz grafica y calculos iniciales"
+    k=0
+    #Llenado panel 1 Parte 1
+    for i in range(0, len(Carac_Area)):
+        Variables_Area.append(StringVar(value=Carac_Area_i[i]))
+        Label(Panel_1, text=Carac_Area[i]).grid(pady=5, row=i, column=0)
+        Entry(Panel_1, width=20, textvariable=Variables_Area[i]).grid(padx=5, row=i, column=1)    
+        k=k+1
+    
+    #Llenado panel 1 Parte 2
+    j=k
+    for i in range(k, k+len(Titulos_Accionamiento)):
+        Label(Panel_1, text=Titulos_Accionamiento[j-i]).grid(pady=5, row=i, column=0)
+        Variables_Accionamiento.append(StringVar(value=Contenido_Accionamiento[j-i]))
+        Entry(Panel_1, width=20, textvariable=Variables_Accionamiento[j-i]).grid(padx=5, row=i, column=1)
+        k=k+1
+        
+    #Llenado panel 1 Parte 3
+    Variables_Molino.append(StringVar(value=Carac_Molino_1[1]))
+    Variables_Molino.append(StringVar(value=Carac_Molino_2[1]))
+    Variables_Molino.append(StringVar(value=Carac_Molino_3[1]))
+    Variables_Molino.append(StringVar(value=Carac_Molino_4[1]))
+    Variables_Molino.append(StringVar(value=Carac_Molino_5[1]))
+    Variables_Molino.append(StringVar(value=Carac_Molino_6[1]))
+        
+    offset_fila_1=k+len(Titulos_Accionamiento)
+    h1=StringVar(value="Modelo_Molino")
+    b1=ttk.Combobox(Panel_1,width=25,values=Carac_Molino_1[1:], textvariable=h1)
+    b1.bind("<<ComboboxSelected>>", Mostrar_molino)
+    h1.set("R 2-S")
+    b1.grid(pady=5, row=offset_fila_1, column=0)
+    
+    Label(Panel_1, text=Carac_Molino_1[0]).grid(pady=5, row=offset_fila_1+1, column=0)
+    Label(Panel_1, text=Carac_Molino_2[0]).grid(pady=5, row=offset_fila_1+2, column=0)
+    Label(Panel_1, text=Carac_Molino_3[0]).grid(pady=5, row=offset_fila_1+3, column=0)
+    Label(Panel_1, text=Carac_Molino_4[0]).grid(pady=5, row=offset_fila_1+4, column=0)
+    Label(Panel_1, text=Carac_Molino_5[0]).grid(pady=5, row=offset_fila_1+5, column=0)
+    Label(Panel_1, text=Carac_Molino_6[0]).grid(pady=5, row=offset_fila_1+6, column=0)    
+    
+    Entry(Panel_1, width=20, textvariable=Variables_Molino[0]).grid(padx=5, row=offset_fila_1+1, column=1) 
+    Entry(Panel_1, width=20, textvariable=Variables_Molino[1]).grid(padx=5, row=offset_fila_1+2, column=1)
+    Entry(Panel_1, width=20, textvariable=Variables_Molino[2]).grid(padx=5, row=offset_fila_1+3, column=1)
+    Entry(Panel_1, width=20, textvariable=Variables_Molino[3]).grid(padx=5, row=offset_fila_1+4, column=1)
+    Entry(Panel_1, width=20, textvariable=Variables_Molino[4]).grid(padx=5, row=offset_fila_1+5, column=1)
+    Entry(Panel_1, width=20, textvariable=Variables_Molino[5]).grid(padx=5, row=offset_fila_1+6, column=1)    
+       
+    #Llenado panel 2
+    for i in range(0, len(Etiquetas_datos_entrada)):
+        Label(Panel_2, text=Etiquetas_datos_entrada[i]).grid(pady=5, row=i, column=0)
+        if (i!=9):
+            Variables_datos_entrada.append(StringVar(value=Valores_iniciales_datos_entrada[i]))
+            Entry(Panel_2, width=20, textvariable=Variables_datos_entrada[i]).grid(padx=5, row=i, column=1)  
+            Label(Panel_2, text=Unidades_datos_entrada[i]).grid(pady=5, row=i, column=2)
+        else:
+            Variables_datos_entrada.append(StringVar(value="Camara"))
+            ttk.Combobox(Panel_2,width=17,values=["Tpcam", "Ward", "Ad"], textvariable=Variables_datos_entrada[i]).grid(pady=5, row=i, column=1)
+            Variables_datos_entrada[i].set("Tpcam")
+            
+    Cantidad_Etapas=StringVar(value="7")
+    Label(Panel_6, text="Cantidad de etapas: ").grid(row=0, column=0)
+    Entry(Panel_6, width=20, textvariable=Cantidad_Etapas).grid(padx=5, row=0, column=1)     
+    #Función para publicar
+    Button(Panel_2, text="Actualizar", command=Actualizar_Valores).grid(pady=5, row=i+1, column=1)     
+        
     #Expansión panel
     Paneles.pack(expand=1, fill='both')
     root.mainloop()
